40f0d68e9788762d3480f82de3d54a271c6471ee
murimolda <murimolda@gmail.com>
2022-05-02 17:37:37 +0300

    feat: add vigenere-cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -25,13 +25,78 @@ class VigenereCipheringMachine {
     this.direct = direct;
   }
 
-  encrypt() {
+  encrypt(message, key) {
 
+    if (typeof (message) === 'undefined' || typeof (key) === 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
 
+    let encryptMessage = '';
+    const alphbet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    if (typeof (this.direct) === 'boolean') {
+      message = message.split('').reverse().join('');
+    }
+    if (message.length > key.length) {
+      let count = 0;
+      for (const char of message) {
+        if (alphbet.includes(char)) {
+          count++;
+        }
+      }
+      key = key.repeat(Math.ceil(count / key.length)).slice(0, count)
+    }
+
+    for (let i = 0, k = 0; i < message.length; i++) {
+      if (alphbet.includes(message[i])) {
+        let index = ((message[i].codePointAt(0) - alphbet.codePointAt(0)) + (key[k].codePointAt(0) - alphbet.codePointAt(0))) % alphbet.length;
+        encryptMessage += alphbet[index];
+        k++;
+      } else {
+        encryptMessage += message[i];
+      }
+    }
+    return encryptMessage;
   }
-  decrypt() {
 
+  decrypt(message, key) {
 
+    if (typeof (message) === 'undefined' || typeof (key) === 'undefined') {
+      throw new Error('Incorrect arguments!');
+    }
+    let decryptMessage = '';
+    const alphbet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    if (typeof (this.direct) === 'boolean') {
+      message = message.split('').reverse().join('');
+    }
+    if (message.length > key.length) {
+      let count = 0;
+      for (const char of message) {
+        if (alphbet.includes(char)) {
+          count++;
+        }
+      }
+      key = key.repeat(Math.ceil(count / key.length)).slice(0, count)
+    }
+
+    for (let i = 0, k = 0; i < message.length; i++) {
+      if (alphbet.includes(message[i])) {
+        let index = ((message[i].codePointAt(0) - alphbet.codePointAt(0)) - (key[k].codePointAt(0) - alphbet.codePointAt(0))) % alphbet.length;
+        if (index >= 0) {
+          decryptMessage += alphbet[index];
+        } else {
+          decryptMessage += alphbet.slice(index, index < -1 ? index + 1 : undefined);
+        }
+        k++;
+      } else {
+        decryptMessage += message[i];
+      }
+    }
+    return decryptMessage;
   }
 }
 
